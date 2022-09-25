a5d5c24030103f1ce01c5f11d3a242cee02606ac
sga-encoder <sga4111@gmail.com>
2022-09-25 10:31:44 -0500

    update home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import { Link } from 'next/link'
 const index = () => {
   return (
-    <div>hols</div>
+    <div className='container'>
+      <Link href={'/crud/crear'}>
+        <div className='d-grid gap-2 w100'>
+          <a className='btn btn-primary m-2'>CRUD</a>
+        </div>
+      </Link>
+    </div>
   )
 }
 
